7685843ee6daa9e02d986ec753a798756ee266b9
Athénaïs Roussel <athenais.roussel@epfedu.fr>
2020-11-03 22:33:29 +0100

    First commit

---
+++ CrowdsourcingWithWords.cs
@@ -0,0 +1,38 @@
+﻿namespace CrowdsourcingWithWords
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+
+	class CrowdsourcingWithWords
+    {
+        /// <summary>
+        /// Main method to run the crowdsourcing experiments presented in Simpson et.al (WWW15).
+        /// </summary>
+        public static void Main()
+        {
+			var data = Datum.LoadData(Path.Combine("Data", "weatherTweets.tsv.gz"));
+
+			// Run model and get results
+			var VocabularyOnSubData = ResultsWords.BuildVocabularyOnSubdata((List<Datum>)data);
+
+            BCCWords model = new BCCWords();
+            ResultsWords resultsWords = new ResultsWords(data, VocabularyOnSubData);
+            DataMappingWords mapping = resultsWords.Mapping as DataMappingWords;
+
+            if (mapping != null)
+            {
+                resultsWords = new ResultsWords(data, VocabularyOnSubData);
+                resultsWords.RunBCCWords("BCCwords", data, data, model, Results.RunMode.ClearResults, true);
+            }
+
+            using (var writer = new StreamWriter(Console.OpenStandardOutput()))
+            {
+                resultsWords.WriteResults(writer, false, false, false, true);
+            }
+
+            Console.WriteLine("Done.  Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
